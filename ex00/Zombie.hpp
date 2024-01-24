975bfa2528ad8a2af8fd9ef848badcb989c88acf
Omar Makran <omakran@c2r7p7.1337.ma>
2024-01-24 11:29:20 +0100

    I'm working on the file.hpp

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 10:46:26 by omakran           #+#    #+#             */
+/*   Updated: 2024/01/24 11:21:38 by omakran          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+    std::string name;
+    
+public:
+    Zombie( std::string name );
+    ~Zombie();
+    void    announce( void );
+    Zombie* newZombie( std::string name );
+    void    randomChump( std::string name );
+};
+
+#endif
