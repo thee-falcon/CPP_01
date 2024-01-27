a1925a70c8b8a64b93037fca44f32ddbd223538e
Omar Makran <omakran@c1r2p9.1337.ma>
2024-01-27 16:48:35 +0100

    ex02 is done, Pointers &Reference

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 19:11:35 by omakran           #+#    #+#             */
-/*   Updated: 2024/01/26 10:17:54 by omakran          ###   ########.fr       */
+/*   Updated: 2024/01/27 16:46:25 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,9 @@ int main(void)
     std::cout << "------------------- Values ------------------- " << std::endl;
     std::cout << "The value of the string variable: " << brain << std::endl;
     std::cout << "The value pointed to by stringPTR: " << *brainPTR << std::endl;
-    std::cout << "The value pointed to by stringREF: " << *(&brainREF) << std::endl;
+    std::cout << " The value pointed to by stringREF: " << brainREF << std::endl;
     // this line of code is the same thing.
-    // std::cout << " The value pointed to by stringREF: " << brainREF << std::endl;
+    // std::cout << "The value pointed to by stringREF: " << *(&brainREF) << std::endl;
+    // but You don't should to do it, because Reference is like a dereferenced pointer. So you don't need to add any symbol to access it.
     return (0);
 }
