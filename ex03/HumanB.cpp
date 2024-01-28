15e4f4d4bd8c6ecb8989a63c1913cbf13fe5382e
Omar Makran <omakran@c1r2p1.1337.ma>
2024-01-28 15:41:13 +0100

    All Humans attack is works, ex03 is done

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 10:50:49 by omakran           #+#    #+#             */
-/*   Updated: 2024/01/28 15:02:03 by omakran          ###   ########.fr       */
+/*   Updated: 2024/01/28 15:09:12 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,12 @@ HumanB::~HumanB()
 void    HumanB::setWeapon( Weapon &weapon)
 {
     this->_weapon = &weapon;
+}
+
+void    HumanB::attack ( void ) const
+{
+    if (this->_weapon != NULL && this->_weapon->getType() != "")
+        std::cout << _name << " attacks with thier " << this->_weapon->getType() << std::endl;
+    else
+        std::cout << _name << " No weapon to attack!" << std::endl;
 }
