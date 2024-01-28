64e5c11b4d2e4903a140931550e3dcde198b1ec6
Omar Makran <omakran@c1r2p1.1337.ma>
2024-01-28 19:42:50 +0100

    Empty string its handled, All Work is clean ALL done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: omakran <omakran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 15:44:51 by omakran           #+#    #+#             */
-/*   Updated: 2024/01/28 19:30:29 by omakran          ###   ########.fr       */
+/*   Updated: 2024/01/28 19:38:43 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,11 @@ int main(int argc, char **argv)
         std::string FileName = argv[1];
         std::string s1 = argv[2];
         std::string s2 = argv[3];
+        if (s1.empty() || s2.empty())
+        {
+            std::cerr << "❌ Error: s1 or s2 cannot be an empty string." << std::endl;
+            return 1;
+        }
         FileReplacer(FileName, s1, s2);
         FileReplacer fileReplacer(FileName, s1, s2);
         fileReplacer.replaceAndSave();
