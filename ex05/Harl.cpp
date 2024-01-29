d599003b8d2117e52c0c657914ab36024c90b87c
thee-falcon <omar.makran.10@gmail.com>
2024-01-29 17:13:25 +0100

    the harl.cpp is going on

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,28 @@
 /*   By: omakran <omakran@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 16:31:56 by omakran           #+#    #+#             */
-/*   Updated: 2024/01/29 16:31:57 by omakran          ###   ########.fr       */
+/*   Updated: 2024/01/29 17:12:09 by omakran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+    
+}
+
+Harl::~Harl()
+{
+
+}
+
+void    Harl::debug( void )
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void    Harl::info( void )
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
